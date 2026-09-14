7accb09a944a7810b0d52903f373afbe30adb4f2
Clarise Tan <2006tanclarise@gmail.com>
2026-09-14 23:13:07 +0800

    Register store and restocks API routes

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import IngredientViewSet, ShakeViewSet, TransactionViewSet
+from .views import IngredientViewSet, ShakeViewSet, TransactionViewSet, StoreViewSet, RestockViewSet
 
 router = DefaultRouter()
 router.register('ingredients', IngredientViewSet)
 router.register('shakes', ShakeViewSet)
 router.register('transactions', TransactionViewSet)
+router.register('store', StoreViewSet)
+router.register('restocks', RestockViewSet)
 
 urlpatterns = router.urls
